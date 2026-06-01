3753809af86a12d70de11401ef905352cd016f98
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-06-01 09:55:44 +0200

    installation de vite

--- site/assets/js/main.js
+++
@@ -1,4 +0,0 @@
-// Fil d'ariane
-// if(document.querySelector(`nav[aria-label="fil d'ariane"] > p`) != null){
-//     document.querySelector(`nav[aria-label="fil d'ariane"] > p`).innerHTML = document.querySelector('h1').innerHTML;
-// }
